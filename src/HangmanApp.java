4c2b3dce1f960f9a5c37bad5eef1d48bdfc3aa38
stewa <stewa@DESKTOP-TSLNNV2>
2023-12-01 16:07:52 -0500

    there was a change maid to the comment section

---
+++ src/HangmanApp.java
@@ -0,0 +1,26 @@
+/*
+author: Alex Stewart
+Date: 11/20/23
+purpose: a program that displays a game of hangman 
+*/
+//file update 
+
+import java.util.Scanner;
+
+public class HangmanApp {
+
+    public static void main(String[] args) {
+        System.out.println("Play the H A N G M A N game");
+        
+        Scanner sc = new Scanner(System.in);        
+        String again = "y";        
+        while(again.equalsIgnoreCase("y")) {
+            var hangman = new Hangman();
+            hangman.drawHangman(7);
+            hangman.playGame();
+            System.out.println();
+            System.out.print("Play again? (y/n): ");
+            again = sc.nextLine();
+        }
+    } 
+}
